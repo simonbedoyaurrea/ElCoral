a08635f8ba1466658c1102cb47e00971cbb5e285
Simon Bedoya <simon.bedoyau@upb.edu.co>
2025-11-27 16:52:47 -0500

    mas arreglos

--- src/Components/CategoryTitle.jsx
+++ src/Components/CategoryTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function CategoryTitle({name,image,description}) {
   return (
-    <div className="relative flex flex-col items-center justify-center rounded-2xl w-11/12 m-auto h-[30dvh] overflow-hidden">
+    <div className="relative flex flex-col items-center justify-center rounded-2xl w-11/12 m-auto h-[30dvh] overflow-hidden p-2">
         <img
             src={image}
             alt={name}
@@ -12,7 +12,7 @@ export default function CategoryTitle({name,image,description}) {
             <h1 className="text-5xl font-bold">{name}</h1>
             <p className="text-lg">{description}</p>
         </div>
-        <div className="absolute inset-0 bg-black/70 z-5" /> 
+        <div className="absolute inset-0 bg-black/60 z-5" /> 
     </div>
 
   )
